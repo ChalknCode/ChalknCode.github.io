c5240c86d4caa3fdb7fb6711a1559d1da0363776
ChalknCode <tk510012@ms9.dyjh.tyc.edu.tw>
2026-09-14 09:21:37 +0800

    config: update API URL to new deployment

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@ const CONFIG = {
   SHEET_ID: '1dtqdnPgVUWuCY-GmKwqK-ZYXXRCURIumiUAB2DFHoug',
   
   // Apps Script 部署後的 Web App URL (用於查詢系統 API)
-  API_URL: 'https://script.google.com/macros/s/AKfycbxf23-zTUmObChWqjTmqsFjCDfnanSUYQWZ9eD4XZlmsKsL0PO7YE4zMN0n64PS2qjr/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbzC3YrmZppCCutlO6CjAD6HJLV04F9_UGMDXPlx4qPjCldIjfHPgq1UUgrmLXQ1k326/exec',
 
   // 工作表名稱
   SHEETS: {
